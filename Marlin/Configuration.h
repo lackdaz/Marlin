d46c727e7ab2dcdcc0ebb949d67cccac1a2ad95e
Seth Loh <lackdaz@gmail.com>
2020-03-28 13:02:57 +0800

    tmc debugging

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -719,7 +719,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 64, 80, 640, 409 } //{ 64, 80, 640, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 64, 80, 640, 205 } //{ 64, 80, 640, 409 }
 
 /**
  * Default Max Feed Rate (mm/s)
